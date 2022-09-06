69b131860513a4c3b19c2782041cccf164034bd5
SKatrin02 <86879264+SKatrin02@users.noreply.github.com>
2022-09-06 17:20:33 +0000

    Add files via upload

---
+++ lb4_Strutinska_Katerina/lb4_Strutinska_Katerina.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int i;
+class Array {
+float *m;
+int n;
+public:
+    Array(int k) {
+    cout << "Constructor with parametr" << endl;
+    n=k;
+    m=new float[n];
+    set_data();
+    }
+~Array() {
+    delete m;
+    cout << "\nDestructor." << endl;
+}
+void set_data() {
+for(i=0;i<n;i++) {
+    if(i%2==0) {
+        m[i]=i+4.1;
+    }
+    else {
+        m[i]=i-1.0;
+    }
+}
+output();
+}
+void output() {
+    cout << "Kolichestvo elementov: " << endl;
+    for(i=0;i<n;i++){
+    cout << m[i] << " ";
+}}
+ 
+friend void sum(Array obj);
+};
+void sum(Array obj) {
+    float sum=0.0;
+cout << "\nDodatni element: "<<obj.m<<"\n";
+for(i=0;i<obj.n;i++) {
+    if(!(i%2)==0) {
+        cout << obj.m[i] << " ";
+        sum=sum+obj.m[i];
+    }
+} cout << endl;
+cout << "Summa dodatnih elementiv: " << sum;
+}
+int main(){
+Array arr(15);
+sum(arr);
+system("pause");
+return 0;
+}
