5e655efeef74257aa0743f50a4fb7b89f8793cd6
SKatrin02 <86879264+SKatrin02@users.noreply.github.com>
2022-11-01 23:42:44 +0200

    Add files via upload

---
+++ lb9_Strutynska_Katerina/Task1_Strutynska_Katerina.cs
@@ -0,0 +1,53 @@
+﻿//
+// Варіант 16(1)
+//
+// Завдання 1 - a=x2+Vz2-y
+//
+// Завдання 2 - Квіткова клумба має форму кола. За заданим радіусом обчислити її периметр і площу
+//
+
+// Початок просторів імен що підключаються
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Кінець просторів імен
+
+namespace Task_1// Оголошення нового простору імен
+{
+    class Program// Оголошення нового класу
+    {
+        static void Main(string[] args)// Оголошення нового методу
+        {
+            int number; // Значення для оператора
+            do// Початок оператора
+            {
+                // Початок задання значень X, Y, Z
+                Console.Write("Set a value X = ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Set a value Y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Set a value Z = ");
+                int z = Convert.ToInt32(Console.ReadLine());
+                // Кінець задання значень
+                number = x; // Прирівнюємо значення для оператора while із значенням X
+                double a = Math.Pow(x, 2) + Math.Sqrt(Math.Pow(y, 2) - y);// Розв'язання формули відповідно варіанту
+                Console.WriteLine($"a = {a}");// Показ результату розв'язання формули
+                // Початок визначення і показу відповідного діапазону Х
+                if (x < -10) Console.WriteLine($"X less than -10");
+                if ((x > -10) && (x < -1)) Console.WriteLine($"X less than -1");
+                if ((x > 1) && (x < 10)) Console.WriteLine($"X more for 1");
+                if (x > 10) Console.WriteLine($"X more for 10");
+                // Кінець визначення і показу діапазону Х
+
+                // Визначення і показ відповідного діпазону результату розв'язання формули
+                if (a < 0) Console.WriteLine($"The result is negative");
+                if (a > 0) Console.WriteLine($"The result is positive");
+                // Кінець визначення і показу діапазону результату
+                Console.ReadKey();// Показ інформації в консолі
+            }
+            while (number != 0);// Перевірка оператором значення X, програма буде повторюватись поки X не буде = 0
+        }
+    }
+}
---
+++ lb9_Strutynska_Katerina/Task2_Strutynska_Katerina.cs
@@ -0,0 +1,33 @@
+﻿//
+// Квіткова клумба має форму кола. За заданим радіусом обчислити її периметр і площу
+//
+// Початок просторів імен що підключаються
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Кінець просторів імен
+
+namespace lb9_Task2_Strutynska_Katerina// Оголошення нового простору імен
+{
+    class Program// Оголошення нового класу
+    {
+        static void Main(string[] args)// Оголошення нового методу
+        {
+            double P;// Периметр
+            double S;// Площа
+            // Початок задання значення радіусу
+            Console.Write("Set a value radius = ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            // Кінець задання значення
+            P = 2 * Math.PI * r;// Формула периметра
+            S = Math.PI * Math.Pow(r, 2);// Формула площі
+            // Початок показу результатів розв'язання формул
+            Console.WriteLine($"Flower bed perimeter = {P}");
+            Console.WriteLine($"Flower bed area = {S}");
+            // Кінець показу результатів
+            Console.ReadKey();// Показ інформації в консолі
+        }
+    }
+}
